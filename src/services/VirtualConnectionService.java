5252b3516340d04c05c3b43e773096150a712db6
Maximilian Etti <maximilian_etti@yahoo.de>
2015-05-14 21:34:27 +0200

    fix input/output setting

--- src/services/VirtualConnectionService.java
+++ src/services/VirtualConnectionService.java
@@ -21,15 +21,28 @@ public class VirtualConnectionService implements ConnectionService
 				System.out.println("Interactive Command Interpreter");
 				System.out.println("   inputs=[hex]      - set inputs");
 				System.out.println("   outputs=[hex]     - set outputs");
+				System.out.println("   inputs            - print inputs");
 				System.out.println("   outputs           - print outputs");
 				while (isRunning) {
 					System.out.print(" > ");
 					String args[] = sc.nextLine().split("=");
 					if (args[0].trim().equals("inputs")) {
 						if (args.length == 1)
+						{
+							String inputString = "|";
+							for (int i = inputs.length - 1; i >= 0; i--)
+								inputString += String.format("%02d|", i);
+
+							inputString += "\n|";
+							for (int i = inputs.length - 1; i >= 0; i--)
+								inputString += (inputs[i] ? " 1|" : " 0|");
+
+							System.out.println(inputString);
 							continue;
+						}
 						setInputs(args[1].trim());
 						System.out.println("set input=" + args[1]);
+
 
 					}
 					else if (args[0].trim().equals("outputs")) {
@@ -56,18 +69,19 @@ public class VirtualConnectionService implements ConnectionService
 
 	void setInputs(String values)
 	{
-		String binary = Integer.toBinaryString(Integer.parseInt(values, 16));
+		int binary = Integer.parseInt(values, 16);
 
-		for (int i = binary.length() - 1; i >= 0; i--)
-			inputs[binary.length() - i - 1] = binary.charAt(i) == '1';
+		for (int i = 0; i < inputs.length; i++)
+			inputs[i] = ((1 << i) & binary) != 0;
+
 	}
 
 	void setOutputs(String values)
 	{
-		String binary = Integer.toBinaryString(Integer.parseInt(values, 16));
+		int binary = Integer.parseInt(values, 16);
 
-		for (int i = binary.length() - 1; i >= 0; i--)
-			outputs[binary.length() - i - 1] = binary.charAt(i) == '1';
+		for (int i = 0; i < outputs.length; i++)
+			outputs[i] = ((1 << i) & binary) != 0;
 	}
 
 	@Override
